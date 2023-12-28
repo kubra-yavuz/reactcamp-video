535b9aebe47e6d375a7dca7cfa4581f06dfce20f
kubranuryavuz <nurkacm@gmail.com>
2023-12-28 14:00:43 +0300

    first commit

---
+++ campproject/src/layouts/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Categories from './Categories';
+import Navi from './Navi';
+import ProductList from '../pages/ProductList';
+
+export default function Dashboard () {
+  return (
+    <div> 
+      
+        <Categories/>
+        <ProductList/>
+
+    </div>
+  );
+}
