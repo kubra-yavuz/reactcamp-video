d1d634d7f8e2874931ed95038cac2dd26776a00f
kubranuryavuz <nurkacm@gmail.com>
2024-01-08 22:08:42 +0300

    Day 13

---
+++ campproject/src/store/reducers/cartReducer.js
@@ -0,0 +1,9 @@
+import { cartItems } from "../initialValues/cartItems";
+
+const initialState = {
+    cartItems:cartItems
+}
+
+export default function cartReducer(state=initialState,{type,payload}){
+
+}
